f5700f33babdb261fa83ce9828551f867da4e1f6
Matthew Lewis <mlewis@ionsol.com>
2025-07-13 18:22:19 +0000

    Update libquad_primer.c
    Cleaned up code

--- libquad_primer.c
+++ libquad_primer.c
@@ -1,48 +1,45 @@
-#include<qauadmath.h> // can be found in the gfortran package
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <quadmath.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-__float128 rad2deg(__float128 rad){
-    return (180.0/M_PIq) * rad;
+static const __float128 RAD_TO_DEG = 180.0Q / M_PIq;
+static const __float128 DEG_TO_RAD = M_PIq / 180.0Q;
+
+__float128 rad2deg(__float128 rad) {
+    return RAD_TO_DEG * rad;
 }
 
-__float128 deg2rad(__float128 deg){
-    return (M_PIq/180.0Q) * deg;
+__float128 deg2rad(__float128 deg) {
+    return DEG_TO_RAD * deg;
 }
 
-void printResult(__float128 result){
-  int  prec = 20;
-  int  width = 46;
-  char buf[128];
-  int n = quadmath_snprintf (NULL, 0, "%+-#46.*Qe", prec, result );
-  if (n > -1)
-  {
-    char *str = malloc (n + 1);
-    if (str)
-    {
-      quadmath_snprintf (str, n + 1, "%+-#46.*Qe", prec, result );
-      printf ("%s\n", str);
+void printResult(__float128 result) {
+    char buf[128];
+    quadmath_snprintf(buf, sizeof(buf), "%+-#46.*Qe", 20, result);
+    printf("%s\n", buf);
+}
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s -d|-r <value>\n", argv[0]);
+        return 1;
     }
-    free (str);
-  }
-}
 
-int main(int argc, char **argv){
-  __float128 deg = 0.0Q;
-  __float128 rad = 0.0Q;
-  if (argc == 3 && strcmp(argv[1],"-d") == 0){
-    rad = strtoflt128(argv[2],NULL);
-    deg = rad2deg(rad);
-    printResult(deg);
-    return 0;
-  }
-  if (argc == 3 && strcmp(argv[1],"-r") == 0){
-    deg = strtoflt128(argv[2],NULL);
-    rad = deg2rad(deg);
-    printResult(rad);
-    return 0;
-  }
-  printf("Please use the form ./is_close -d 2.12 or ")
-  return 1;
+    __float128 val = strtoflt128(argv[2], NULL);
+    if (isnanq(val)) {
+        fprintf(stderr, "Error: Invalid input.\n");
+        return 1;
+    }
+
+    if (strcmp(argv[1], "-d") == 0) {
+        printResult(rad2deg(val));
+        return 0;
+    } else if (strcmp(argv[1], "-r") == 0) {
+        printResult(deg2rad(val));
+        return 0;
+    }
+
+    fprintf(stderr, "Error: Invalid option. Use -d or -r.\n");
+    return 1;
 }
